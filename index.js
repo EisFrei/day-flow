6ad14b27d9332a3cc4cbd4e962510389d04f70ce
Martin Freisen <martin.freisen@comspace.de>
2019-06-05 18:01:18 +0200

    add jsdoc

--- index.js
+++ index.js
@@ -5,17 +5,99 @@ const TYPE = {
 	SUNSET: 1
 };
 
+/**
+ * Helps determine the flow of the day/night cycle
+ *
+ * @class DayFlow
+ * @author martin.freisen@gmail.com
+ * @param {number} lat The geolocation's latitude.
+ * @param {number} lon The geolocation's longitude.
+ * @param {Date} date The date that the day/night cycle will correspond to.
+ * @throws Will throw an error, if the parameters have the wrong type.
+ * 
+ */
 class DayFlow {
 	constructor(lat, lon, date = new Date()) {
 		if (typeof lat !== 'number' || typeof lon !== 'number') {
 			throw new Error('lat and lon have to be numbers');
 		}
+		
+		if (!(date instanceof Date)) {
+			throw new Error('date is not a date');
+		}		
+		/** 
+		 * The geolocation's latitude.
+		 * @member {number}
+		 */
 		this.lat = lat;
+
+		/** 
+		 * The geolocation's longitude.
+		 * @member {number}
+		 */
 		this.lon = lon;
+
+		/** 
+		 * The date that the day/night cycle will correspond to.
+		 * @member {Date}
+		 */
 		this.date = date;
+
+		/** 
+		 * True if date is in the day part of the cycle.
+		 * @member {boolean}
+		 */
+		this.isDay = null;
+
+		/** 
+		 * True if date is in the night part of the cycle.
+		 * @member {boolean}
+		 */
+		this.isNight = null;
+
+		/** 
+		 * Time of the sunrise.
+		 * @member {Date}
+		 */
+		this.sunrise = null;
+
+		/** 
+		 * Time of the sunset.
+		 * @member {Date}
+		 */
+		this.sunset = null;
+
+		/** 
+		 * Milliseconds since the last sunrise/sunset.
+		 * @member {number}
+		 */
+		this.msSinceChange = null;
+
+		/** 
+		 * Milliseconds until the next sunrise/sunset.
+		 * @member {number}
+		 */
+		this.msUntilChange = null;
+
+		/** 
+		 * Length of the current day/night in milliseconds
+		 * @member {number}
+		 */
+		this.msCycleLength = null;
+
+		/** 
+		 * Percentage of the current day/night that has already passed
+		 * @member {number}
+		 */
+		this.cyclePercentage = null;
 		this._calculate();
 	}
 
+	/** 
+	 * Used Internally. Gets the sunrise and sunset of a specified date.
+	 * @method
+	 * @returns {Object}
+	 */
 	_getDateCycleChanges(date) {
 		return [{
 			date: SunriseSunset.getSunrise(this.lat, this.lon, date),
@@ -26,6 +108,11 @@ class DayFlow {
 		}];
 	}
 
+	/** 
+	 * Used Internally. Gets a list of sunrises and sunsets of the set date and the neighboring dates.
+	 * @method
+	 * @returns {Array}
+	 */
 	_getAllCycleChanges() {
 		const changes = [];
 		for (let i = -1; i <= 1; i++) {
@@ -36,6 +123,10 @@ class DayFlow {
 		return changes;
 	}
 
+	/** 
+	 * Used internally. Calculates all of the member variables.
+	 * @method
+	 */
 	_calculate() {
 		const cycleChanges = this._getAllCycleChanges();
 		let pastChange = null;
@@ -53,7 +144,7 @@ class DayFlow {
 		this.isDay = pastChange.type === TYPE.SUNRISE;
 		this.isNight = pastChange.type === TYPE.SUNSET;
 
-		if (pastChange.type === TYPE.SUNRISE) {
+		if (this.isDay) {
 			this.sunrise = pastChange.date;
 			this.sunset = futureChange.date;
 		} else {
